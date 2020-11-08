c27202b4f9e01e74746a10fda48e20b68d9e167a
KhanhPham05 <68932626+KhanhPham05@users.noreply.github.com>
2020-11-08 14:27:04 +0700

    fix

--- src/ForLoopTest.java
+++
@@ -1,8 +0,0 @@
-public class ForLoopTest {
-	public static void main (String args[]) {
-	int i,j ;
-	for (i=1;i<=10;i++) { System.out.println("i="+ i); System.out.println("a");}
-	System.out.println();
-	for (j=1;j<=10;++j) { System.out.println("j="+ j); System.out.println("a");}
-   }
-}
