55a36b9c8f1b337950aa73ad6884613d306ab8f9
KhanhPham05 <68932626+KhanhPham05@users.noreply.github.com>
2020-11-09 20:07:48 +0700

    TheJava-2 update

--- src/DateCount/MainDateCount.java
+++ src/DateCount/MainDateCount.java
@@ -4,28 +4,22 @@ import java.util.Scanner;
 
 public class MainDateCount {
 	public static void main(String[] args) {
-		//int date, month, year;
-
+		DateCountMethod ob  = new DateCountMethod() ;
 		//TODO: Insert D-M-Y
 		System.out.println("Nhap ngay thang nam: ");
 
 		System.out.print("\t Ngay: ");
 		Scanner sc = new Scanner(System.in);
+
 		int date = sc.nextInt();
+		System.out.print("\t Thang: "); int month = sc.nextInt(); ob.getMonth(month) ;
 
+		System.out.print("\t Nam: "); int year = sc.nextInt();
 
-		System.out.print("\t Thang: ");
-		int month = sc.nextInt();
-
-
-
-		System.out.print("\t Nam: ");
-		int year = sc.nextInt();
-
-		System.out.println("\t"+date + "/"+month+"/"+year);
+		//System.out.println("\t"+date + "/"+month+"/"+ year);
+		
 		//TODO: Calculating Method Call
-		DateCountMethod ob  = new DateCountMethod() ;
-		ob.getDayMonthYear(date,month,year);
+		ob.checkYearDayMonth();
 		ob.LeapYear();
 		ob.calc();
 	}
